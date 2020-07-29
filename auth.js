38de9832fad3a7bc3cfe204afbcd5574eb995999
Ranjith-kumar03 <ranjithkumar03.1980@gmail.com>
2020-07-29 19:55:10 +0530

    initial commit

---
+++ auth.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+
+
+const accessTokenSecret = 'youraccesstokensecret';
+
+const refreshTokenSecret = 'yourrefreshtokensecrethere';
+const refreshTokens = [];
+
+
+
+
+app.listen(3000, () => {
+    console.log('Authentication service started on port 3000');
+});
+
+
+const users = [
+    {
+        username: 'john',
+        password: 'password123admin',
+        role: 'admin'
+    }, {
+        username: 'anna',
+        password: 'password123member',
+        role: 'member'
+    }
+];
+
+
+
+/*app.post('/login', (req, res) => {
+    // Read username and password from request body
+    const { username, password } = req.body;
+
+    // Filter user from the users array by username and password
+    const user = users.find(u => { return u.username === username && u.password === password });
+
+    if (user) {
+        // Generate an access token
+        const accessToken = jwt.sign({ username: user.username,  role: user.role }, accessTokenSecret);
+
+        res.json({
+            accessToken
+        });
+    } else {
+        res.send('Username or password incorrect');
+    }
+});*/
+
+
+app.post('/login', (req, res) => {
+    // read username and password from request body
+    const { username, password } = req.body;
+
+    // filter user from the users array by username and password
+    const user = users.find(u => { return u.username === username && u.password === password });
+
+    if (user) {
+        // generate an access token
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+        const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
+
+        refreshTokens.push(refreshToken);
+
+        res.json({
+            accessToken,
+            refreshToken
+        });
+    } else {
+        res.send('Username or password incorrect');
+    }
+});
+
+
+app.post('/token', (req, res) => {
+    const { token } = req.body;
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    if (!refreshTokens.includes(token)) {
+        return res.sendStatus(403);
+    }
+
+    jwt.verify(token, refreshTokenSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+
+        res.json({
+            accessToken
+        });
+    });
+});
+
+
+app.post('/logout', (req, res) => {
+    const { token } = req.body;
+    let refreshTokens = refreshTokens.filter(token => t !== token);
+
+    res.send("Logout successful");
+});
